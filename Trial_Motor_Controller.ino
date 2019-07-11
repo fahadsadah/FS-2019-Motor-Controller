3c179823c840d26265b5b09b42289e060e488b57
Fahad Sadah <92872+fahadsadah@users.noreply.github.com>
2019-07-11 18:04:30 +0100

    ready to drive noise

--- Trial_Motor_Controller.ino
+++ Trial_Motor_Controller.ino
@@ -1,4 +1,5 @@
-#include <TimerOne.h>
+#include <TimerOne/TimerOne.h>
+#include <Arduino.h>
 
 #define currentSensor 0
 #define apps1 1
@@ -10,33 +11,11 @@
 #define motorOutput 5
 #define speaker 6
 
-int soundTimer = 0;
+volatile int soundTimer = 0;
 volatile bool readyToDrive = false;
 
-void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-
-    pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
-    pinMode(A2, INPUT);
-
-    pinMode(readyToDriveSwitch, INPUT);
-    pinMode(readyToDriveLight, OUTPUT);
-    pinMode(brakeLightSwitch, INPUT);
-    pinMode(motorOutput, OUTPUT);
-    pinMode(speaker, OUTPUT);
-
-    TCCR0B = TCCR0B & B11111000 | B00000001; // for PWM frequency of 62500.00 Hz
-
-    attachInterrupt(digitalPinToInterrupt(2), checkReadyToDriveSwitch, CHANGE);
-
-    Timer1.initialize(1500000);
-    Timer1.attachInterrupt(timer);
-}
-
-void checkReadyToDriveSwitch() {
-    if (digitalRead(readyToDriveSwitch) == LOW) { // RTD switched off
+void readyToDriveChange() {
+    if (digitalRead(readyToDriveSwitch) == LOW) {
         analogWrite(motorOutput, 0); //cut the power
         digitalWrite(speaker, LOW); //cancel the ready sound
 
@@ -52,40 +31,68 @@ void checkReadyToDriveSwitch() {
 }
 
 void timer() {
-    if (soundTimer == 0) {
-        digitalWrite(speaker, LOW);
-    } else {
-        soundTimer--;
+    if (soundTimer > 0) {
+        soundTimer = soundTimer - 1;
+        if (soundTimer == 0) {
+            digitalWrite(speaker, LOW);
+        }
     }
+}
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
+    pinMode(A2, INPUT);
+
+    pinMode(readyToDriveSwitch, INPUT);
+    pinMode(readyToDriveLight, OUTPUT);
+    pinMode(brakeLightSwitch, INPUT);
+    pinMode(motorOutput, OUTPUT);
+    pinMode(speaker, OUTPUT);
+
+    digitalWrite(brakeLightSwitch, LOW);
+    digitalWrite(readyToDriveSwitch, LOW);
+
+    TCCR0B = TCCR0B & B11111000 | B00000001; // for PWM frequency of 62500.00 Hz
+
+    attachInterrupt(0, readyToDriveChange, CHANGE); //pin D2
+
+    Timer1.initialize(1500000l);
+    Timer1.attachInterrupt(timer);
+
+    Serial.println("Hello");
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
 
-    float V_1 = ((1/5)*1023);
-    float V_1075 = ((1.075/5)*1023);
-    float V_25= ((2.5/5)*1023);
-    float V_3 = ((3/5)*1023);
+    float V_1 = ((1 / 5) * 1023);
+    float V_1075 = ((1.075 / 5) * 1023);
+    float V_25 = ((2.5 / 5) * 1023);
+    float V_3 = ((3 / 5) * 1023);
 
     int currentSens = analogRead(currentSensor); //Read current sensor voltage from pin 0
     int accelPot1 = analogRead(apps1); //Read pedal pot 1 voltage
     int accelPot2 = analogRead(apps2); //Read pedal pot 2 voltage
     int powerReq = 0; //Set power requested to 0
 
-    float accelPot1Trav= ((accelPot1/1023.0)*100); //Calculate pedal pot 1 travel %
-    float accelPot2Trav= ((accelPot2/1023.0)*100); //Calculate pedal pot 2 travel %
-    float currentSensPerc = (((currentSens-V_25)/V_1075)*100); //Calculate current draw %
+    float accelPot1Trav = ((accelPot1 / 1023.0) * 100); //Calculate pedal pot 1 travel %
+    float accelPot2Trav = ((accelPot2 / 1023.0) * 100); //Calculate pedal pot 2 travel %
+    float currentSensPerc = (((currentSens - V_25) / V_1075) * 100); //Calculate current draw %
 
-    float powerReq1 = accelPot1Trav-currentSensPerc;//Calculate total requested current
+    float powerReq1 = accelPot1Trav - currentSensPerc;//Calculate total requested current
     powerReq = map(powerReq1, 0, 100, 184, 654); //0.9V to 3.2V
 
     if (readyToDrive) {
         analogWrite(motorOutput, powerReq / 4);
     }
 
-    float vOut = powerReq*(5.0/1023);
-    float ped = accelPot1*(5.0/1023);
-    float sens = currentSens*(5.0/1023);
+    float vOut = powerReq * (5.0 / 1023);
+    float ped = accelPot1 * (5.0 / 1023);
+    float sens = currentSens * (5.0 / 1023);
 
 /*
   Serial.print("Accel % = ");
@@ -94,11 +101,12 @@ void loop() {
   Serial.println(sens);
   Serial.print("Power Delivery = ");
   Serial.println(vOut);
- */
+
 
     Serial.print(ped);Serial.print(",");
     Serial.print(sens);Serial.print(",");
     Serial.print(vOut);Serial.println("");
+*/
+    delay(1000);
 
-    delay(2);
 }
